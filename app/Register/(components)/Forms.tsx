b99ddea23d0c48d6bc2b0dea6f890e6d9e9d3633
Nagi2880 <alexanderamaral17@gmail.com>
2025-08-08 20:26:17 -0300

    Register form bases finish

--- app/Register/(components)/Forms.tsx
+++ app/Register/(components)/Forms.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
-import Label from '../../(elements)/'
+import Formfield from '../../(elements)/Formfield'
 const Forms = () => {
     return (
-        <div className='login-forms'>
-            <form>
-            
-
-
-
-
-
-
-                {/*<Label htmlFor="name" content="Name" className="form-label" />
-                <Label htmlFor="surname" content="Surname" className="form-label" />
-                <Label htmlFor="username" content="Username" className="form-label" />
-                <Label htmlFor="password" content="Password" className="form-label" />
-                <Label htmlFor="confirm-password" content="Confirm Password" className="form-label" />
-                <Label htmlFor="email" content="Email" className="form-label" />
-                <Label htmlFor="phone" content="Phone" className="form-label" />
-                <Input type="text" id="country" name="country" placeholder="Country" /> Here will be an autocomplete }
-                <Input type="date" id="birthdate" name="birthdate" placeholder="Birthdate" />*/
-                }
+        <div className='register-forms-container'>
+            <form className='register-forms'>
+                <Formfield type="text" id="name" name="name" placeholder="Name" htmlFor='name' content='Name' />
+                <Formfield type="text" id="surname" name="surname" placeholder="Surname" htmlFor='surname' content='Surname' />
+                <Formfield type="text" id="username" name="username" placeholder="Username" htmlFor='username' content='Username' />
+                <Formfield type="password" id="password" name="password" placeholder="Password" htmlFor='password' content='Password' />
+                <Formfield type="password" id="confirm-password" name="confirm-password" placeholder="Confirm Password" htmlFor='confirm-password' content='Confirm Password' />
+                <Formfield type="email" id="email" name="email" placeholder="Email" htmlFor='email' content='Email' />
+                <Formfield type="tel" id="phone" name="phone" placeholder="Phone" htmlFor='phone' content='Phone' />
+                <Formfield type="text" id="country" name="country" placeholder="Country" htmlFor='country' content='Country' />
+                <Formfield type="date" id="birthdate" name="birthdate" placeholder="Birthdate" htmlFor='birthdate' content='Birthdate' />
+                {/* Add any additional fields as necessary for a example "i'm not a bot checkbox" */  }
+                <input type="submit" value="Register" className='register-submit'> Register </input>
             </form> 
         </div>
     )
