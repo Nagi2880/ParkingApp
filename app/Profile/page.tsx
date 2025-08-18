c6b1e617a3efee1f02607ba599857d89d7617831
Nagi2880 <alexanderamaral17@gmail.com>
2025-08-17 22:13:59 -0300

    profile page created and started buttons

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Button from '../(elements)/Button'
 function page() {
   return (
-    <div>page</div>
+    <div className={'Profile container'}>
+      <ul>
+        <Button href='' className='Maindata-button' >Profile</Button>
+        <Button href='' className='Maindata-button'>Personal information</Button>
+        <Button href='' className='Maindata-button'>Acoount secure</Button>
+        <Button href='' className='Maindata-button'>Notifications{ /* bell icon */} </Button>
+        <Button href='' className='Maindata-button'>Privacy {/* Cars/parkings/history travel*/} </Button>
+        <Button href='' className='Maindata-button'>Language{/* Flag icon */} </Button>
+      </ul>
+    </div>
   )
 }
 
